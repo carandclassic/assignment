afb4350558d67eb310dec48c37ead6b6b83b519d
Ricardo Banegas <ricardo@frontend.nu>
2023-05-23 23:59:41 +0200

    Skeleton for VueJS

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -1,3 +1,22 @@
 <template>
-    Hello world
+    <Sort></Sort>
+    <Cars></Cars>
 </template>
+
+<style>
+    body {
+        font-family: 'figtree';
+    }
+</style>
+
+<script>
+    import Cars from './Cars.vue';
+    import Sort from './Sort.vue';
+
+    export default {
+        components: {
+            Cars,
+            Sort
+        }
+    }
+</script>
