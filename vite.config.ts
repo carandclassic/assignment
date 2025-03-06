784f153a6a19e17c8342bb9c0290e9ec1de68f37
Bram Ceulemans <bram@ceulemans.dev>
2025-03-06 22:23:54 +0000

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.ts',
             refresh: true,
         }),
         vue({
