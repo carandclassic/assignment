011d7bfcd311b40ca9c4bf2f9cc0098fba91d45e
Bram Ceulemans <bram@ceulemans.dev>
2025-03-06 19:34:32 +0000

    Update to Laravel 12

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,1 @@
-import {createApp} from "vue";
-
-import Home from './Pages/Home.vue';
-
-const app = createApp({});
-
-app.component('home-page', Home);
-
-app.mount("#app");
+import './bootstrap';
