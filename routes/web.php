bf921ad7c516bc81edf2ddbb5385e741f7e7db5f
Bram Ceulemans <bram@ceulemans.dev>
2025-09-25 00:00:10 +0100

    Add laravel 12

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', HomeController::class);
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
